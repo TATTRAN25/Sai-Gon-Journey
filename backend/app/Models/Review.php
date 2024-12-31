d85c525f5de285050595e75bbca50da85a22aa3c
Tran Anh Tuan <trananhtuan.tdc.2223@gmail.com>
2024-12-31 13:15:47 +0700

    add model for backend

---
+++ backend/app/Models/Review.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'tour_id', 'hotel_id', 'comment', 'rating',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tour()
+    {
+        return $this->belongsTo(Tour::class);
+    }
+
+    public function hotel()
+    {
+        return $this->belongsTo(Hotel::class);
+    }
+}
